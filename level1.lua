58e538f830d2e70229b5c1eaf9557920be5bfb43
Lars Preben Sørsdahl <lars@preben.no>
2013-04-24 15:47:54 +0200

    Refactoring and added score panel.

--- level1.lua
+++ level1.lua
@@ -1,116 +1,150 @@
+local gravity = 2
+local distanceToWall = 100
+
+
 local storyboard = require("storyboard")
 local scene = storyboard.newScene()
 local physics = require "physics"
 local physicsData = (require "shapedefs").physicsData(1.0)
 
-physics.start()
-physics.setGravity(0,2)
-
---physics.pause()
-
 local screenW, screenH, halfW = display.contentWidth, display.contentHeight, display.contentWidth*0.5
 local balls = {}
 local ballIndex = 1
 
-local activeBall1 = nil
+local activeBall
+local score = 0
+local scoreText
+local equationText
 
 function scene:createScene(event)
-    print(screenW)
-    print(screenH)
-    
-    local background = display.newImage("glassbg.png", true)    
-    
+    setupPhysics()
+    setupBackground()
+    setupWalls()
+    setupScorePanel()
+    setupEquationPanel()
+end
+
+function setupPhysics()
+    physics.start()
+    physics.setGravity(0,gravity)
+    physics.setDrawMode("normal")
+end
+
+function setupBackground()
+    local background = display.newImage("glassbg.png", true)       
+end
+
+function setupWalls()
+    staticMaterial = {density=2, friction=.3, bounce=.4}  
+
     local floor = display.newRect(0, screenH, screenW, screenH)  
     local lWall = display.newRect(0, 0, 0,  screenH)
-    local rWall = display.newRect(screenW, 0, screenW, screenH)  
-    staticMaterial = {density=2, friction=.3, bounce=.4}  
+    local rWall = display.newRect(screenW, 0, screenW, screenH)
     physics.addBody(floor, "static", staticMaterial)  
     physics.addBody(lWall, "static", staticMaterial)  
     physics.addBody(rWall, "static", staticMaterial)  
-    
 end
 
-local activate = function( event )
+function setupScorePanel()
+    scoreText = display.newText(score, 0, 0, native.systemFont, 12*2)
+    scoreText:setReferencePoint(display.TopRightReferencePoint)
+    scoreText.x = screenW - 5
+end
+
+function setupEquationPanel()
+    equationText = display.newText("X + Y = 10", 0, 0, native.systemFont, 12*2)
+    equationText:setReferencePoint(display.TopRightReferencePoint)
+    equationText.x = screenW - 5
+    equationText.y = 30
+end
+
+function addToScore(value)
+    score = score + value
+    scoreText.text = score
+end
+
+local ballPressed = function( event )
     local t = event.target
     local phase = event.phase
-    
-
     
     if "began" == phase then
         local myIndex = t.myIndex
         local ball = balls[myIndex]
-        if activeBall1 == nil then
-            activeBall1 = balls[myIndex]
-        elseif ball ~= activeBall1 then
-            
-            if activeBall1.value + ball.value == 10 then
-                print("Yey")
-                activeBall1:removeSelf()
-                ball:removeSelf()
-            end
-            activeBall1 = nil
+        if activeBall == nil then
+            selectX(ball)
+        elseif ball ~= activeBall then
+            selectY(ball)
         end
-       -- print( "removing ball #" .. myIndex )
-       -- balls[myIndex]:removeSelf() -- destroy joint
+        -- print( "removing ball #" .. myIndex )
+        -- balls[myIndex]:removeSelf() -- destroy joint
     end
     
     -- Stop further propagation of touch event
     return true
 end
 
-function scene:enterScene( event )
-    local group = self.view
+
+
+function selectX(ball)
+    activeBall = ball
+    equationText.text = ball.value .. " + Y = 10"
 end
 
-function scene:exitScene( event )
-    local group = self.view	
+function selectY(ball)
+    if activeBall.value + ball.value == 10 then
+        addToScore(1)
+        activeBall:removeSelf()
+        ball:removeSelf()
+        equationText.text = activeBall.value .. " + " .. ball.value .. " = 10"
+    else
+        equationText.text = activeBall.value .. " + " .. ball.value .. " != 10"
+    end
+    activeBall = nil
 end
-
-function scene:destroyScene( event )
-    local group = self.view
-end
-
-function addEventListeners()
-    scene:addEventListener( "createScene", scene )
-    scene:addEventListener( "enterScene", scene )
-    scene:addEventListener( "exitScene", scene )
-    scene:addEventListener( "destroyScene", scene )
-end
-
-addEventListeners()
 
 function onCollision( event )
     if ( event.phase == "began" ) then
 
         print( "began: " .. event.selfElement.myName .. " & " .. event.otherElement.myName )
-
+        
     elseif ( event.phase == "ended" ) then
-
+        
         print( "ended: " .. event.selfElement.myName .. " & " .. event.otherElement.myName )
     end
 end
 
 -- create a random new object
-function newItem()	
-    num = math.random(0,9)
+function dropBall()
+    num = math.random(1,9)
     obj = display.newImage(num..".png");
-    physics.addBody( obj, physicsData:get("orange"))	
-    
+    physics.addBody( obj, physicsData:get("orange"))
+    obj.isFixedRotation = true
+
     -- random start location
-    obj.x = 60 + math.random( screenW-60 )
-    obj.y = -60
+    obj.x = distanceToWall + math.random( screenW-distanceToWall )
+    obj.y = -distanceToWall
     
     -- add collision handler
-   -- obj.collision = onLocalCollision
+    -- obj.collision = onLocalCollision
     obj.myName = "Foobar"
     obj.value = num
-    obj:addEventListener( "touch", activate )
-  --  obj:addEventListener( "collision", onCollision)
+    obj:addEventListener( "touch", ballPressed )
+    --  obj:addEventListener( "collision", onCollision)
     balls[ballIndex] = obj
     obj.myIndex = ballIndex
     ballIndex = ballIndex + 1
 end
 
-local dropCrates = timer.performWithDelay( 2000, newItem, 30 )
+function addEventListeners()
+    scene:addEventListener( "createScene", scene )
+end
 
+function dropBalls()
+    local dropCrates = timer.performWithDelay( 2000, dropBall, 30 )
+end
+
+
+-- Main script
+dropBalls()
+addEventListeners()
 return scene
